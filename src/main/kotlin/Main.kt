514e07f93b831730a04e33d97433e9aa61fe103a
orishaba-mercy <orishabamercybanga@gmail.com>
2023-03-09 08:57:15 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,77 @@
+import kotlin.math.min
+
+fun main() {
+   println(schools("Akirachix","hilside"))
+    var numbers = arrayOf(1,2,3,4,5,)
+    println(numbers.min())
+    println(numbers.max())
+    println(numbers.average())
+  println(myself("Mercy"))
+
+
+
+
+
+
+
+
+
+
+
+}
+
+//    Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length. (4 pts)
+fun schools(name1:String,name2: String):String{
+    var characters=("Akirachix,hilside")
+    return characters
+
+
+
+
+}
+
+
+
+
+//2. Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements (7 pts)
+fun numbers(number1: Int,number2: Int,number3:Int,number4:Int,number5: Int):Int{
+    var numbers = arrayOf(1,2,3,4,5,)
+   return numbers.min()
+    return numbers.max()
+}
+
+
+
+//3. Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output. (3 pts)
+fun myself(name:String){
+    var myself="${name[0]},${name[1]},${name[2]},${name[3]},$name[4]"
+}
+
+
+
+//4. Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it. (4 pts)
+fun num(name:String){
+    var num = arrayOf("jane","mary")
+    var numb = arrayOf("hope","glow")
+    println(arrayOf(num) .plus(arrayOf(numb)))
+}
+
+
+
+
+
+
+//5. Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+var =
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+//Instantiate your calculator object and invoke all its functions
+//
+//(12 pts)
